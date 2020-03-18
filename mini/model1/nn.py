3aec7dd6170a09431b601caa7458ca4a3e91fe5a
Karthik Viswanath S <47050339+karthikviz2000@users.noreply.github.com>
2020-03-18 12:37:01 +0530

    Add files via upload

---
+++ mini/model1/nn.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from sklearn import metrics
+
+data=pd.read_csv("/home/karthik/Desktop/mini/model1/altered_dataset2/train.csv",header=None,skiprows=1)
+
+df=DataFrame(data)
+
+x=df.iloc[:, 0:3]
+y_amt=df.iloc[:, 3:4]
+y_waste=df.iloc[:, 4:5]
+children=pd.DataFrame(df.iloc[:, 0:1])
+adults=pd.DataFrame(df.iloc[:, 1:2])
+elderly=pd.DataFrame(df.iloc[:, 2:3])
+
+
+#for testing
+data_test=pd.read_csv("/home/karthik/Desktop/mini/model1/altered_dataset2/test.csv",header=None,skiprows=1)
+df_test=DataFrame(data_test)
+ytest=df_test.iloc[:, 3:4]
+xtest=df_test.iloc[:, 0:3]
+
+#for cross validation
+data_crossvalidation=pd.read_csv("/home/karthik/Desktop/mini/model1/altered_dataset2/cross.csv",header=None,skiprows=1)
+df_crossvalidation=DataFrame(data_crossvalidation)
+ycross=df_crossvalidation.iloc[:, 3:4]
+xcross=df_crossvalidation.iloc[:, 0:3]
+
+
+output_amt=[]
+output_test=[]
+output_crossvalidation=[]
+
+
+#hyperparameters
+no_neurons=5
+m=x.shape[1]                                                  
+n=y_amt.shape[1]                                              
+no_examples=x.shape[0]
+no_iter=500
+alpha=0.00001
+
+
+#x=np.transpose(x)
+
+w=np.random.rand(m*no_neurons).reshape(no_neurons,m)        
+b=np.ones((no_neurons,n))                                     
+w_wasted=np.random.rand(m*no_neurons).reshape(no_neurons,m)        
+
+
+
+
+def eqn(x,theta,bias):
+	ans=np.dot(x,theta)+bias
+	#print(ans)
+	return ans.reshape(x.shape[0],1)
+
+def cost_func(m,y,pred):
+	squared_error=np.square(pred-y)
+	ans=np.sum(squared_error)/(2*m)
+	#print(ans)
+	return np.array(ans)[0]
+
+def differential(m,y,pred,x):
+	ans=(np.sum(np.dot(np.transpose(pred-y),x)))/m
+	#print(ans)
+	return ans
+
+
+def gradient_descent(m,x,y,theta,no_iter,alpha,bias):
+	ind=[]
+	cost=[]
+	for i in range(no_iter):
+		pred=eqn(x,theta,bias)
+		gradient0=differential(m,y,pred,children)
+		gradient1=differential(m,y,pred,adults)
+		gradient2=differential(m,y,pred,elderly)
+		theta[0]=theta[0]-(alpha*gradient0)
+		theta[1]=theta[1]-(alpha*gradient1)
+		theta[2]=theta[2]-(alpha*gradient2)
+		#print(theta,cost_func(m,y,pred))
+	return theta.reshape(1,3)
+
+
+for i in range(no_neurons):
+	w[i]=gradient_descent(no_examples,x,y_amt,np.transpose(w[i]),no_iter,alpha,b[i])
+	#w_wasted[i]=gradient_descent(no_examples,x,y_waste,np.transpose(w_wasted[i]),no_iter,0.000001,b[i])
+
+for j in range(0,no_neurons):
+	output_amt.append(np.transpose(eqn(x,np.transpose(w[j]),b[j])))
+	#output_wasted.append(np.transpose(eqn(x,np.transpose(w_wasted[j]),b[j])))
+	output_test.append(np.transpose(eqn(xtest,np.transpose(w[j]),b[j])))
+	output_crossvalidation.append(np.transpose(eqn(xcross,np.transpose(w[j]),b[j])))
+
+
+#print(w)
+#print(w_wasted)
+
+
+#print("rmse of 5 neurons for training data:\n\n")
+
+#for i in range(0,no_neurons):
+#	print(np.sqrt(metrics.mean_squared_error(y_amt,np.transpose(output_amt[i]))))
+
+
+#print("rmse of 5 neurons for testing data:\n\n")
+#for i in range(0,no_neurons):
+#	print(np.sqrt(metrics.mean_squared_error(ytest,np.transpose(output_test[i]))))
+	#print(np.sqrt(metrics.mean_squared_error(y_waste,np.transpose(output_wasted[i]))))
+
+
+#print("rmse of 5 neurons for cross validation data:\n\n")
+#for i in range(0,no_neurons):
+#	print(np.sqrt(metrics.mean_squared_error(ycross,np.transpose(output_crossvalidation[i]))))
+
+#print("weights are:\n")
+#for i in range(0,no_neurons):
+#	print(w[i])
+#	print("\n")
+
+print(output_amt)
+print("\n\n")
+
+print(output_crossvalidation)
+
+#print(np.array(y_amt))
+#print("\n\n")
+#print(output_amt)
+#print("\n\n")
+#print(output_wasted[0])
