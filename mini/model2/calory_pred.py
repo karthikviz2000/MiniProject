a713ac5e0c8e46d81e4191e917986bc01c333697
karthikviz2000 <karthikviz2000@gmail.com>
2020-05-27 07:05:15 +0530

    added working code

---
+++ mini/model2/calory_pred.py
@@ -0,0 +1,7 @@
+calory={'gulab':225,'friedRice':319,'whiteRice':281,'biriyani':290,'chapathi':171,'vegCurry':169,
+'channaMasala':130,'pickle':10,'chips':194,'sweet':304,'pakoda':122,'iceCream':173,'savouries':245}
+
+print(calory)
+
+
+
